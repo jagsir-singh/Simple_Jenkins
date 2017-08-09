4588dd98c531788248ec8dfe0b436471e361b8ce
Jagsir-Singh <Jagsir.Singh@coxautoinc.com>
2017-08-09 11:46:35 -0400

    simple testing with Jenkins

---
+++ Simple_Jenkins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Simple_Jenkins.Startup))]
+namespace Simple_Jenkins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
